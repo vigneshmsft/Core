04386f2cc0224de0cb22177992269052cf956936
vigneshmsft <vignesh.n@outlook.com>
2019-09-04 20:30:02 +0100

    adding Core.Loggin.Serilog

---
+++ Core.Logging.Serilog/LoggerFactory.cs
@@ -0,0 +1,109 @@
+﻿namespace Core.Logging.Serilog
+{
+    using System;
+    using global::Serilog.Events;
+    using global::Serilog;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.WindowsServer.TelemetryChannel;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class LoggerFactory
+    {
+        private static readonly bool IsTraceEnabled = TraceEnabled();
+
+        public static ILog CreateFileLogger(string logFilePath)
+        {
+            var serilogger = new LoggerConfiguration().MinimumLevel.Verbose()
+                    .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7)
+                    .CreateLogger();
+
+            return new SeriLogger(serilogger);
+        }
+
+        public static ILog CreateApplicationInsightsLogger(IServiceCollection services, string instrumentationKey, string appName = "")
+        {
+            var telemetryClient = new TelemetryClient
+            {
+                InstrumentationKey = instrumentationKey
+            };
+
+            services.AddApplicationInsightsTelemetry(options =>
+            {
+                options.InstrumentationKey = instrumentationKey;
+                options.ApplicationVersion = appName;
+            });
+
+            var telemetryConfiguration = new TelemetryConfiguration(instrumentationKey, new ServerTelemetryChannel());
+
+            var serilogger = new LoggerConfiguration().MinimumLevel.Verbose()
+                .WriteTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces)
+                .Filter.ByExcluding(ShouldExcludeLogging)
+                .CreateLogger();
+
+            return new SeriLogger(serilogger, telemetryClient, appName);
+        }
+
+        public static ILog CreateApplicationInsightsLogger(string instrumentationKey, string appName = "")
+        {
+            var telemetryClient = new TelemetryClient
+            {
+                InstrumentationKey = instrumentationKey
+            };
+
+            var telemetryConfiguration = new TelemetryConfiguration(instrumentationKey, new ServerTelemetryChannel());
+
+            var serilogger = new LoggerConfiguration().MinimumLevel.Verbose()
+                    .WriteTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces)
+                    .Filter.ByExcluding(ShouldExcludeLogging)
+                    .CreateLogger();
+
+            return new SeriLogger(serilogger, telemetryClient, appName);
+        }
+
+        public static ILog CreateApplicationInsightsAndFileLogger(string logFilePath, string instrumentationKey, string appName = "")
+        {
+            var telemetryClient = new TelemetryClient
+            {
+                InstrumentationKey = instrumentationKey
+            };
+
+            var telemetryConfiguration = new TelemetryConfiguration(instrumentationKey, new ServerTelemetryChannel());
+
+            var serilogger = new LoggerConfiguration().MinimumLevel.Verbose()
+                    .WriteTo.File(logFilePath)
+                    .WriteTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces)
+                    .Filter.ByExcluding(ShouldExcludeLogging)
+                    .CreateLogger();
+
+            return new SeriLogger(serilogger, telemetryClient, appName);
+        }
+
+        public static ILog CreateConsoleFileLogger()
+        {
+
+            var serilogger = new LoggerConfiguration().MinimumLevel.Verbose()
+                                .Enrich.FromLogContext()
+                                .WriteTo.Console().CreateLogger();
+
+            return new SeriLogger(serilogger);
+        }
+
+        private static bool ShouldExcludeLogging(LogEvent logEvent)
+        {
+            if (logEvent.Level == LogEventLevel.Information ||
+                logEvent.Level == LogEventLevel.Error ||
+                logEvent.Level == LogEventLevel.Fatal)
+            {
+                return false;
+            }
+
+            return !IsTraceEnabled;
+        }
+
+        private static bool TraceEnabled()
+        {
+            return Environment.GetEnvironmentVariable("TraceEnabled")?.Equals("true", StringComparison.CurrentCultureIgnoreCase) ?? false;
+        }
+    }
+}
---
+++ Core.Logging.Serilog/Serilogger.cs
@@ -0,0 +1,87 @@
+﻿namespace Core.Logging.Serilog
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Concurrent;
+    using global::Serilog;
+    using Microsoft.ApplicationInsights;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    public class SeriLogger : ILog, IDisposable
+    {
+        private readonly ILogger _logger;
+        private readonly TelemetryClient _telemetryClient;
+        private readonly string _appName;
+
+        public SeriLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        internal SeriLogger(ILogger logger, TelemetryClient telemetryClient, string appName) : this(logger)
+        {
+            _telemetryClient = telemetryClient;
+            _appName = appName;
+        }
+
+        public void Request(RequestTrace requestTrace)
+        {
+            var requestTelemetry = new RequestTelemetry(requestTrace.Route, requestTrace.StartTime,
+                requestTrace.Duration, requestTrace.ResponseCode, requestTrace.Successful);
+            AddAppName(requestTelemetry.Properties);
+            _telemetryClient?.TrackRequest(requestTelemetry);
+        }
+
+        public void Trace(string trace)
+        {
+            _logger.Verbose(trace);
+        }
+
+        public void Debug(string debug)
+        {
+            _logger.Debug(debug);
+        }
+
+        public void Warn(string warning)
+        {
+            _logger.Warning(warning);
+            _telemetryClient?.TrackEvent("Warning", AddAppName(new Dictionary<string, string> { { "Message", warning } }));
+        }
+
+        public void Info(string information)
+        {
+            _logger.Information(information);
+        }
+
+        public void Error(string error, IDictionary<string, string> parameters = null)
+        {
+            _logger.Error(error);
+            _telemetryClient?.TrackException(new Exception(error), AddAppName(parameters));
+        }
+
+        public void Error(Exception exception, IDictionary<string, string> parameters = null)
+        {
+            _logger.Error(exception, $"Exception {exception}");
+            _telemetryClient?.TrackException(exception, AddAppName(parameters));
+        }
+
+        public void Event(string @event, IDictionary<string, string> parameters = null)
+        {
+            _telemetryClient?.TrackEvent(@event, AddAppName(parameters));
+        }
+
+        public void Dispose()
+        {
+            _telemetryClient?.Flush();
+        }
+
+        private IDictionary<string, string> AddAppName(IDictionary<string, string> properties)
+        {
+            if (properties == null)
+                properties = new ConcurrentDictionary<string, string>();
+
+            properties["App"] = _appName;
+            return properties;
+        }
+    }
+}
